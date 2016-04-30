9a09168c92f0a571e8ba4c720db159a544503794
stevenroach7 <sroach@macalester.edu>
2016-04-30 11:24:09 -0500

    Added the dynamic programming solution

---
+++ src/patchOGrass/MainApp.java
@@ -0,0 +1,7 @@
+package patchOGrass;
+
+/**
+ * Created by Steven on 4/21/16.
+ */
+public class MainApp {
+}
--- src/patchOGrass/PatchOGrass.java
+++ src/patchOGrass/PatchOGrass.java
@@ -1,5 +1,7 @@
 package patchOGrass;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Random;
 
 /**
@@ -9,11 +11,157 @@ import java.util.Random;
 
 public class PatchOGrass {
 
+
+
+
     public static void main(String arg[]){
-        float[][] grass = makeGrass(5,5);
-        System.out.println(" ");
-        float[][] bestPatch = surveyGrass(grass, 2, 2);
+//        float[][] grass = makeGrass(5,5);
+//        System.out.println(" ");
+//        float[][] bestPatch = surveyGrass(grass, 2, 2);
+
+//        int[] test = new int[]{2,4,7,0, 4};
+//        int[] answer = findMaxRectangleHistogram(test);
+//        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
+
+        int[][] matrix = new int[5][5];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if ((i) < 4 ) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        int[] results = findMaxSubmatrix1s(matrix);
+        for (int i=0; i<results.length; i++) {
+            System.out.println(results[i]);
+        }
+
     }
+
+
+
+    public static int[] findMaxSubmatrix1s(int[][] matrix) {
+
+        int[] frontier = new int[matrix.length];
+        int maxArea = 0;
+        int[] bestCorners = new int[]{0,0,0,0,0,0,0,0};
+
+        for (int y = 0; y < matrix.length; y++) { // TODO: Is this right???
+            // Update frontier
+            for (int i = 0; i < matrix[y].length; i++) {
+                if (matrix[y][i] == 0) { // Make frontier value at index 0 regardless of what it was before.
+                    frontier[i] = 0;
+                } else {
+                    frontier[i] += 1; // Increment frontier value at index i by 1.
+                }
+            }
+            // Frontier represents a histogram
+            int[] maxHist = findMaxRectangleHistogram(frontier); // Find max rectangle of frontier histogram
+            int[] corners = findCorners(maxHist[0]+maxHist[2], y, maxHist[1], maxHist[2]);
+            int area = findArea(maxHist);
+
+            if (area > maxArea) {
+                bestCorners = corners;
+                maxArea = area;
+            }
+        }
+        System.out.println(maxArea);
+        return bestCorners;
+    }
+
+    private static int findArea(int[] corners) {
+
+        return corners[1] * corners[2];
+    }
+
+    private static int[] findCorners(int x, int y, int width, int height) {
+        int[] corners =  new int[8];
+        corners[0] = x;
+        corners[1] = y;
+        corners[2] = x + width;
+        corners[3] = y;
+        corners[4] = x;
+        corners[5] = y + height;
+        corners[6] = x + width;
+        corners[7] = y + height;
+        return corners;
+    }
+
+
+
+    private static int[] findMaxRectangleHistogram(int[] histValues) {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int maxArea = 0;
+        int[] maxInfo = new int[] {0,0,0}; // Holds an index, a width and a height.
+
+        int i; // Declaring i here allows us to only increment i if the first if statement in the loop is executed.
+        for (i = 0; i < histValues.length;) {
+
+            if (stack.isEmpty() || histValues[i] >= histValues[stack.peek()]) // Only increment i if stack is empty or current val is < top stack val
+                stack.push(i++);
+            else {
+                int value = stack.pop();
+
+                int area; // Calculate new area
+                boolean emptyStack;
+                if (stack.isEmpty()) {
+                    area = histValues[value] * i;
+                    emptyStack = true;
+                } else {
+                    area =  histValues[value] * (i - stack.peek() - 1);
+                    emptyStack = false;
+                }
+                if (area > maxArea) { // Decompose this step
+                    if (emptyStack) {
+                        maxInfo[0] = 0;
+                        maxInfo[1] = i;
+                        maxInfo[2] = histValues[value];
+                    } else {
+                        maxInfo[0] = stack.peek() + 1;
+                        maxInfo[1] = (i - stack.peek() - 1);
+                        maxInfo[2] = histValues[value];
+                    }
+                    maxArea = area;
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            int value = stack.pop();
+
+            int area; // Calculate new area
+            boolean emptyStack;
+            if (stack.isEmpty()) {
+                area = histValues[value] * histValues.length;
+                emptyStack = true;
+            } else {
+                area =  histValues[value] * (histValues.length - stack.peek() - 1);
+                emptyStack = false;
+            }
+            if (area > maxArea) {
+                if (emptyStack) {
+                    maxInfo[0] = 0;
+                    maxInfo[1] = i;
+                    maxInfo[2] = histValues[value];
+                } else {
+                    maxInfo[0] = stack.peek() + 1;
+                    maxInfo[1] = (i - stack.peek() - 1);
+                    maxInfo[2] = histValues[value];
+
+                }
+                maxArea = area;
+            }
+        }
+
+        //System.out.println("Max area is " + maxArea);
+        return maxInfo;
+    }
+
+
+
 
 
     /**
@@ -50,8 +198,8 @@ public class PatchOGrass {
         double currentBest = 0;
         int besti = 0;
         int bestj = 0;
-        for (int i=0; i<=(field.length-n-1); i++){ // TODO: Fix these indices
-            for(int j=0; j<=(field[0].length-m-1); j++){
+        for (int i=0; i<=(field.length-n); i++){ // TODO: Fix these indices
+            for(int j=0; j<=(field[0].length-m); j++){
                 float currentPatch = sumGrass(field, i, j, n, m);
                 System.out.println(currentPatch);
                 if (currentPatch > currentBest){
@@ -77,7 +225,7 @@ public class PatchOGrass {
      */
     public static float sumGrass(float[][] field, int currentI, int currentJ, int n, int m){
         float total = 0;
-        for (int i = currentI; i<=n-1; i++){
+        for (int i = currentI; i<=currentI + n; i++){
             for (int j = currentJ; j<=m-1; j++){
                 total+=field[i][j];
             }
