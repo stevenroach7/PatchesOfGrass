a0edc090f150dc1f68fd059c6c8c52102b2612b9
stevenroach7 <sroach@macalester.edu>
2016-04-30 12:00:59 -0500

    Synchronized BF and DP methods.

--- src/patchOGrass/PatchOGrass.java
+++ src/patchOGrass/PatchOGrass.java
@@ -1,8 +1,6 @@
 package patchOGrass;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Steven on 4/5/16.
@@ -23,27 +21,39 @@ public class PatchOGrass {
 //        int[] answer = findMaxRectangleHistogram(test);
 //        System.out.println(answer[0] + " " + answer[1] + " " + answer[2]);
 
-        int[][] matrix = new int[5][5];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if ((i) < 4 ) {
-                    matrix[i][j] = 1;
-                } else {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+//        int[][] matrix = new int[5][5];
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                if ((i) < 4 ) {
+//                    matrix[i][j] = 1;
+//                } else {
+//                    matrix[i][j] = 0;
+//                }
+//            }
+//        }
+//
+//
+//        for (int i=0; i<results.length; i++) {
+//            System.out.println(results[i]);
+//        }
 
-        int[] results = findMaxSubmatrix1s(matrix);
-        for (int i=0; i<results.length; i++) {
-            System.out.println(results[i]);
-        }
 
 
 
+        int[][] grass = makeBinaryGrass(10,10);
+        System.out.println(" ");
+        HashMap bestPatch = surveyBinaryGrassBF(grass);
+
+
+        HashMap bestPatchDP = findMaxSubmatrix1s(grass);
 
 
     }
+
+
+
+
+
 
 
     /**
@@ -51,11 +61,18 @@ public class PatchOGrass {
      * @param matrix, a 2D array with only 0's or 1's as entries
      * @return a Map holding an upper left coordinate, a height, and width. 
      */
-    public static int[] findMaxSubmatrix1s(int[][] matrix) {
+    public static HashMap<String, Integer>  findMaxSubmatrix1s(int[][] matrix) {
+
+        HashMap<String, Integer> results = new HashMap<String, Integer>();
+        results.put("height", 0);
+        results.put("width", 0);
+        results.put("x", 0);
+        results.put("y", 0);
+
 
         int[] frontier = new int[matrix.length];
         int maxArea = 0;
-        int[] bestCorners = new int[]{0,0,0,0,0,0,0,0};
+        //int[] bestCorners = new int[]{0,0,0,0,0,0,0,0};
 
         for (int y = 0; y < matrix.length; y++) { // TODO: Is this right???
             // Update frontier
@@ -68,40 +85,35 @@ public class PatchOGrass {
             }
             // Frontier represents a histogram
             int[] maxHist = findMaxRectangleHistogram(frontier); // Find max rectangle of frontier histogram
-            int[] corners = findCorners(maxHist[0]+maxHist[2], y, maxHist[1], maxHist[2]);
+
+            //int[] corners = findCorners(maxHist[0]+maxHist[2], y, maxHist[1], maxHist[2]);
             int area = findArea(maxHist);
 
             if (area > maxArea) {
-                bestCorners = corners;
                 maxArea = area;
+                results.put("height", maxHist[2]);
+                results.put("width", maxHist[1]);
+                results.put("x", maxHist[0]);
+                results.put("y", y - maxHist[2]+1);
             }
         }
-        System.out.println(maxArea);
-        return bestCorners;
+        System.out.println(" " + results.get("height") + " by " + results.get("width") + ": starting at (" + results.get("x") + "," + results.get("y") + ").");
+        return results;
     }
 
-    private static int findArea(int[] corners) {
-        return corners[1] * corners[2];
+    private static int findArea(int[] histResults) {
+        return histResults[1] * histResults[2];
     }
 
-    private static int[] findCorners(int x, int y, int width, int height) {
-        int[] corners =  new int[8];
-        corners[0] = x;
-        corners[1] = y;
-        corners[2] = x + width;
-        corners[3] = y;
-        corners[4] = x;
-        corners[5] = y + height;
-        corners[6] = x + width;
-        corners[7] = y + height;
-        return corners;
-    }
+//    private static HashMap<String, Integer> findCorners(int x, int y, int width, int height) {
+//        new
+//    }
 
 
 
     private static int[] findMaxRectangleHistogram(int[] histValues) {
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> stack = new LinkedList<>();
         int maxArea = 0;
         int[] maxInfo = new int[] {0,0,0}; // Holds an index, a width and a height.
 
@@ -171,92 +183,83 @@ public class PatchOGrass {
 
 
 
+
     /**
      * Creates a field of grass with each blade being a random length between 0 and 1.
-     * @param N, an int height of the field of grass.
+     *@param N, an int height of the field of grass.
      * @param M, an int width of the field of grass
-     * @return A two dimensional array of floats representing the field of grass.
+     * @return A two dimensional array of binary 1s and 0s representing the field of grass.
      */
-    public static float[][] makeGrass(int N, int M){
+    public static int[][] makeBinaryGrass(int N, int M){
         Random rand = new Random();
-        float[][] field = new float[N][M];
+        int[][] field = new int[N][M];
         for (int i = 0; i<N; i++){
             for (int j=0; j<M; j++){
-                float randomNum = rand.nextFloat();
-                randomNum = (float) ( Math.round(randomNum * 1000d) / 1000d);
-                System.out.print(randomNum + " ");
-                field[i][j] = randomNum;
+                int oneOrZero = rand.nextInt(10) % 2;
+                System.out.print(oneOrZero + " ");
+                field[i][j] = oneOrZero;
             }
             System.out.println(" ");
         }
         return field;
     }
 
-
-    /**
-     * Searches through the field of grass with a brute-force approach and finds the n by m subfield
-     * of grass with the highest average height.
-     * @param field, A two dimensional array of floats representing the field of grass.
-     * @param n, an int height of the subfield of grass
-     * @param m, an int width of the subfield of grass
-     * @return A two dimensional array of floats representing the optimal subfield of grass.
-     */
-    public static float[][] surveyGrass(float[][] field, int n, int m){
-        double currentBest = 0;
-        int besti = 0;
-        int bestj = 0;
-        for (int i=0; i<=(field.length-n); i++){ // TODO: Fix these indices
-            for(int j=0; j<=(field[0].length-m); j++){
-                float currentPatch = sumGrass(field, i, j, n, m);
-                System.out.println(currentPatch);
-                if (currentPatch > currentBest){
-                    currentBest = currentPatch;
-                    besti = i;
-                    bestj = j;
-                }
-            }
-        }
-        System.out.println("best patch found at [" + besti +"][" + bestj + "] with a total of " + currentBest);
-        return focusGrass(field, besti, bestj, n, m);
-    }
-
-
-    /**
-     * Sums the lengths of a subfield of grass.
-     * @param field, A two dimensional array of floats representing the field of grass.
-     * @param currentI, an int representing the vertical coordinate of the upper left corner of a subfield of grass.
-     * @param currentJ, an int representing the horizontal coordinate of the upper left corner of a subfield of grass.
-     * @param n, an int height of the subfield of grass.
-     * @param m, an int width of the subfield of grass.
-     * @return total, a float sum of the lengths of the subfield of grass.
-     */
-    public static float sumGrass(float[][] field, int currentI, int currentJ, int n, int m){
-        float total = 0;
-        for (int i = currentI; i<=currentI + n; i++){
-            for (int j = currentJ; j<=m-1; j++){
+    public static int sumBinaryGrass(int[][] field, int currentI, int currentJ, int h, int w){
+        int total = 0;
+        for (int i = currentI; i< currentI + h; i++){
+            for (int j = currentJ; j< currentJ + w; j++){
                 total+=field[i][j];
             }
         }
+        //System.out.print(" adds to : " + total);
+        //System.out.println(" ");
         return total;
     }
 
-
-    /**
-     * Returns a subfield of grass.
-     * @param field, A two dimensional array of floats representing the field of grass.
-     * @param currentI, an int representing the vertical coordinate of the upper left corner of a subfield of grass.
-     * @param currentJ, an int representing the horizontal coordinate of the upper left corner of a subfield of grass.
-     * @param n,  an int height of the subfield of grass.
-     * @param m,  an int width of the subfield of grass.
-     * @return patch, a two dimensional array of floats representing the subfield of grass.
-     */
-    public static float[][] focusGrass(float[][] field, int currentI, int currentJ, int n, int m) {
-        float[][] patch = new float[n][m];
-        for (int i = currentI; i <= n-1; i++) {
-            System.arraycopy(field[i], currentJ, patch[i], currentJ, m - currentJ);
+    public static HashMap<String, Integer> surveyBinaryGrassBF(int[][] field){
+        HashMap<String, Integer> results = new HashMap<String, Integer>();
+        results.put("height", 0);
+        results.put("width", 0);
+        results.put("i", 0);
+        results.put("j", 0);
+        int best = 0;
+        int current;
+        int totLoops = 0;
+        for (int i=0; i<field.length;i++){
+            for (int j=0; j<field[0].length; j++){
+                for (int h=1; h<= field.length - i; h++){
+                    for (int w=1; w<= field[0].length - j; w++){
+                        totLoops++;
+                        //System.out.println(i + " " + j + " " + h + " " + w);
+                        current = sumBinaryGrass(field, i, j, h, w);
+                        if (current == (h * w)){
+                            if (current > best){
+                                results.put("height", h);
+                                results.put("width", w);
+                                results.put("i", i);
+                                results.put("j", j);
+                                best = h * w;
+                            }
+                        }
+                    }
+                }
+            }
         }
-        return patch;
+        System.out.println(totLoops);
+        System.out.println(" " + results.get("height") + " by " + results.get("width") + ": starting at (" + results.get("j") + "," + results.get("i") + ").");
+        return results;
     }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
