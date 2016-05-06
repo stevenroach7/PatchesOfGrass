4f4b1d5a51a11958715a1f457c0b99a89083ec6d
mhagen94 <mhagen2@macalester.edu>
2016-05-06 13:55:15 -0500

    Histogram mostly working

--- src/patchOGrass/Histogram.java
+++ src/patchOGrass/Histogram.java
@@ -22,9 +22,9 @@ public class Histogram extends GCompound {
         this.N = N;
 
         for (int i=0; i<frontier.length; i++){
-            GRect column = createColumn(M);
+            GRect column = createColumn(frontier[i]);
             add(column);
-            column.setLocation(400 + (i* Blade.WIDTH + Field.GAP_SPACING * i), M-(frontier[i] * Blade.HEIGHT));
+            column.setLocation(400 + (i* Blade.WIDTH + Field.GAP_SPACING * i), (M * Blade.HEIGHT)-(frontier[i] * Blade.HEIGHT) + Field.GAP_SPACING);
         }
     }
 
@@ -34,7 +34,7 @@ public class Histogram extends GCompound {
      * @return
      */
     private GRect createColumn(int h) {
-        int height = h * (Blade.HEIGHT + Field.GAP_SPACING) + Field.GAP_SPACING;
+        int height = h * (Blade.HEIGHT) + Field.GAP_SPACING;
         int width = Blade.WIDTH;
 
         GRect column = new GRect(width, height);
